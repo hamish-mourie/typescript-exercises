b1313215b5a06b387e62db0b29ab101bd959a38a
Hamish Mourie <20253849+hamish-mourie@users.noreply.github.com>
2022-10-12 14:32:31 +1300

    Add additional parameter type checks

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase, result);
+    }
 }
 var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var showResult = true;
+var resultPhrase = 'The result is:';
+add(number1, number2, showResult, resultPhrase);
